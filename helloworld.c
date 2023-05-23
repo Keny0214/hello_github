93709a46011421e023488cd065aa71a1dcc58c9e
yamaga <d083vgu@yamaguchi-u.ac.jp>
2023-05-23 16:38:31 +0900

    山鹿が変更を加えました。

--- helloworld.c
+++ helloworld.c
@@ -4,6 +4,7 @@ int main(void){
 	printf("Hello World! \n");
 	printf("My hobby is singing songs. \n");
 	printf("I am in branch2nd. \n");
+	printf("2388060839 山鹿寛貴がコラボレーターとして加わりました。\n");
 
 	return(0);
 }
